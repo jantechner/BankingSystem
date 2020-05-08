28ce960f08d7c5bb363165262f8d1b1179d4ca9b
MarcinKrz <marokrzemo245@gmail.com>
2020-05-08 21:07:42 +0200

    added unit test "Outgoing transfer"

--- BankingSystem.Tests/UnitTest1.cs
+++ BankingSystem.Tests/UnitTest1.cs
@@ -37,5 +37,28 @@ namespace BankingSystem.Tests
 
             Assert.Equal(10000, account1.Loans[0].RemainingAmount);
         }
+        
+        
+        [Fact]
+        public void PlainAccount_Outgoing_transfer()
+        {
+            var globalBank = new Bank("PKO BP", "PL", "BPKOPLPW");
+            var millenium = new Bank("Millenium", "PL", "MILL");
+
+            InterBankPaymentManager.RegisterBank(globalBank);
+            InterBankPaymentManager.RegisterBank(millenium);
+            
+            var customer1 = new Customer("87040500342") {Name = "Jan", Surname = "Kowalski"};
+            var customer2 = new Customer("97021500531") {Name = "Grzegorz", Surname = "Nowak"};
+            customer1.Open<DebitAccount>(globalBank);
+            customer2.Open<PlainAccount>(millenium);
+            var account1 = customer1.GetAccounts()[0];
+            var account2 = customer2.GetAccounts()[0];
+            globalBank.Execute(new IncreaseBalance(account1, 1000));
+            globalBank.Execute(new OutgoingTransfer(account1, "97021500531", 200));
+            InterBankPaymentManager.ExecuteTransfers();
+            Assert.Equal(200,account2.Balance);
+            
+        }
     }
 }
