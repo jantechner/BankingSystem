72c4225863d6e7ae484ed32f96f9433b44dbef5c
MarcinKrz <marokrzemo245@gmail.com>
2020-05-08 19:24:52 +0200

    added unit test "get Loan and check Remaining amount"

--- BankingSystem.Tests/UnitTest1.cs
+++ BankingSystem.Tests/UnitTest1.cs
@@ -22,5 +22,20 @@ namespace BankingSystem.Tests
             var exception = Assert.Throws<Exception>(() => account.DecreaseBalance(100));
             Assert.Equal("Not enough funds", exception.Message);
         }
+        
+        [Fact]
+        public void PlainAccount_GetLoan()
+        {
+            var globalBank = new Bank("PKO BP", "PL", "BPKOPLPW");
+
+            InterBankPaymentManager.RegisterBank(globalBank);
+            var customer1 = new Customer("87040500342") {Name = "Jan", Surname = "Kowalski"};
+            customer1.Open<DebitAccount>(globalBank);
+            var account1 = customer1.GetAccounts()[0];
+            
+            Console.WriteLine(customer1.RequestLoan(account1, 10000, globalBank));
+
+            Assert.Equal(10000, account1.Loans[0].RemainingAmount);
+        }
     }
 }
