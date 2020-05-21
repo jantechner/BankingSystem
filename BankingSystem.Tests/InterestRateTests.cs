4485425e18465b4561677013b1a268ae7c0941b0
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2020-05-21 21:06:31 +0200

    Regular and debit accounts tests

--- BankingSystem.Tests/InterestRateTests.cs
+++ BankingSystem.Tests/InterestRateTests.cs
@@ -19,7 +19,7 @@ namespace BankingSystem.Tests
         {
             var bank = new Bank("testBank", "", "");
             var customer = new Customer("");
-            customer.Open<DebitAccount>(bank);
+            customer.Open<RegularAccount>(bank);
             var accout = customer.Get<Account>()[0];
             customer.DepositMoney(accout, 1000);
             bank.Execute((new CalculateInterest(accout)));
@@ -33,7 +33,7 @@ namespace BankingSystem.Tests
         {
             var bank = new Bank("testBank", "", "");
             var customer = new Customer("");
-            customer.Open<DebitAccount>(bank);
+            customer.Open<RegularAccount>(bank);
             var accout = customer.Get<Account>()[0];
             customer.DepositMoney(accout, 1000);
             bank.Execute(new ChangeInterestRate(accout, new AnotherInterestRate(0.02)));
@@ -47,6 +47,48 @@ namespace BankingSystem.Tests
         {
             // Simplified compound interest calculation
             // 10 periods with capitalization on the end of each period
+            var bank = new Bank("testBank", "", "");
+            var customer = new Customer("");
+            customer.Open<RegularAccount>(bank);
+            var accout = customer.Get<Account>()[0];
+            customer.DepositMoney(accout, 1000);
+            bank.Execute(new ChangeInterestRate(accout, new AnotherInterestRate(0.02)));
+            for (var i = 0; i < 10; i++)
+            {
+                bank.Execute((new CalculateInterest(accout)));
+            }
+            Assert.Equal(Math.Round(1000 * Math.Pow(1.02, 10), 10), Math.Round((accout.Balance), 10));
+        }
+        
+        [Fact]
+        public void InterestRate_DebitCalculateDefaultRate()
+        {
+            var bank = new Bank("testBank", "", "");
+            var customer = new Customer("");
+            customer.Open<DebitAccount>(bank);
+            var accout = customer.Get<Account>()[0];
+            customer.DepositMoney(accout, 1000);
+            bank.Execute((new CalculateInterest(accout)));
+            Assert.Equal(1050, accout.Balance);
+        }
+
+        [Fact]
+        public void InterestRate_DebitCalculateCustomRate()
+        {
+            var bank = new Bank("testBank", "", "");
+            var customer = new Customer("");
+            customer.Open<DebitAccount>(bank);
+            var accout = customer.Get<Account>()[0];
+            customer.DepositMoney(accout, 1000);
+            bank.Execute(new ChangeInterestRate(accout, new AnotherInterestRate(0.02)));
+            bank.Execute((new CalculateInterest(accout)));
+            _output.WriteLine($"accout.Balance: {accout.Balance}");
+            Assert.Equal(1020, accout.Balance);
+        }
+
+        [Fact]
+        public void InterestRate_DebitCalculateCompoundInterest()
+        {
             var bank = new Bank("testBank", "", "");
             var customer = new Customer("");
             customer.Open<DebitAccount>(bank);
